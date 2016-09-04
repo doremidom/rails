12660091c88776b0bc14c6cb6f69f59b03177d8e
Dominique Star <dominiquexstar@gmail.com>
2016-09-04 16:37:28 +0200

    advanced functionality add

--- sinatra/web_guesser.rb
+++ sinatra/web_guesser.rb
@@ -2,13 +2,21 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 number = rand(100)
+
+if @guesses == 0
+	number = rand(100)
+	@guesses = 5
+end
+
 message = ""
+@@guesses = 5
 
 def check_guess(guess, number)
-	if guess == ""
-		return message = ""
+	if guess.nil?
+		message = "empty"
 	else
 		guess = guess.to_i
+		
 		if guess > number
 			if guess - number > 5
 				message = "Way tooooo high! #{guess}"
@@ -24,14 +32,26 @@ def check_guess(guess, number)
 		else
 			message = "You got it right!"
 		end
+		@@guesses -= 1
 	end
+
 	message
 end
 
 
 get '/' do
+	puts @@guesses
 	guess = params["guess"]
 	message = check_guess(guess, number)
+	if @@guesses == 0
+		number = rand(100)
+		@@guesses = 5
+		message = "You've lost, the number has been reset."
+	elsif message == "You got it right!"
+		number = rand(100)
+		@@guesses = 5
+		message = "You got it right! Number has been reset."
+	end
 	erb :index, :locals => {:number => number, :message => message}
 end
 

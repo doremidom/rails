006c094f24dae71e865306651139effadb92bf21
Dominique Star <dominiquexstar@gmail.com>
2016-09-04 16:16:25 +0200

    implement colors

--- sinatra/web_guesser.rb
+++ sinatra/web_guesser.rb
@@ -1,27 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(1..100)
+number = rand(100)
 message = ""
 
 def check_guess(guess, number)
-	guess = guess.to_i
-	if guess == 0
-		message = ""
-	elsif guess > number
-		if guess - number > 5
-			message = "Way too high!"
-		else
-			message = "Too high!"
-		end
-	elsif guess < number
-		if number - guess > 5
-			message = "Way too low!"
-		else
-			message = "Too low!"
-		end
+	if guess == ""
+		return message = ""
 	else
-		message = "You got it right!"
+		guess = guess.to_i
+		if guess > number
+			if guess - number > 5
+				message = "Way tooooo high! #{guess}"
+			else
+				message = "Too high!"
+			end
+		elsif guess < number
+			if number - guess > 5
+				message = "Way too low!"
+			else
+				message = "Too low!"
+			end
+		else
+			message = "You got it right!"
+		end
 	end
 	message
 end
